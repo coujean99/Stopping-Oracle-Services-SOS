8db1e9632c65927c0f612647ea49fdd9fcf6e525
Jean-Christophe Couture <54377330+coujean99@users.noreply.github.com>
2022-06-03 00:28:59 -0400

    Add files via upload
    Only in french for now. you can add a language support if you want.

---
+++ ServicesOracle/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace ServicesOracle
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_auDemarrage = new System.Windows.Forms.Button();
+            this.btn_etat = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_auDemarrage
+            // 
+            this.btn_auDemarrage.Location = new System.Drawing.Point(56, 12);
+            this.btn_auDemarrage.Name = "btn_auDemarrage";
+            this.btn_auDemarrage.Size = new System.Drawing.Size(218, 23);
+            this.btn_auDemarrage.TabIndex = 0;
+            this.btn_auDemarrage.Text = "(Au démarrage)";
+            this.btn_auDemarrage.UseVisualStyleBackColor = true;
+            this.btn_auDemarrage.Click += new System.EventHandler(this.btn_auDemarrage_Click);
+            // 
+            // btn_etat
+            // 
+            this.btn_etat.Location = new System.Drawing.Point(56, 55);
+            this.btn_etat.Name = "btn_etat";
+            this.btn_etat.Size = new System.Drawing.Size(218, 23);
+            this.btn_etat.TabIndex = 1;
+            this.btn_etat.Text = "(État)";
+            this.btn_etat.UseVisualStyleBackColor = true;
+            this.btn_etat.Click += new System.EventHandler(this.btn_etat_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(337, 93);
+            this.Controls.Add(this.btn_etat);
+            this.Controls.Add(this.btn_auDemarrage);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Services Oracle arrière plan";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Button btn_auDemarrage;
+        private Button btn_etat;
+    }
+}
---
+++ ServicesOracle/Form1.cs
@@ -0,0 +1,89 @@
+using System.Diagnostics;
+using System.Security.Principal;
+using System.ServiceProcess;
+
+namespace ServicesOracle
+{
+    public partial class Form1 : Form
+    {
+        private bool serviceActifAuDemarrage;
+        private bool serviceActif;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            ServiceController serviceOracleAuDemarrage = new ServiceController("OracleServiceXE");
+
+            if (serviceOracleAuDemarrage.StartType == ServiceStartMode.Automatic)
+                serviceActifAuDemarrage = true;
+            else
+                serviceActifAuDemarrage = false;
+
+            if (serviceOracleAuDemarrage.Status == ServiceControllerStatus.Running)
+                serviceActif = true;
+            else
+                serviceActif |= false;
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (serviceActifAuDemarrage == true)
+                btn_auDemarrage.Text = "Désactiver les services au démarrage";
+            else
+                btn_auDemarrage.Text = "Activer les services au démarrage";
+
+            if (serviceActif == true)
+                btn_etat.Text = "Désactiver les services";
+            else
+                btn_etat.Text = "Activer les services";
+        }
+
+
+        private void btn_auDemarrage_Click(object sender, EventArgs e)
+        {
+            var servicesOracle =
+                from sc in ServiceController.GetServices()
+                where sc.ServiceName.StartsWith("OracleOra") || sc.ServiceName.Equals("OracleServiceXE")
+                select sc;
+
+            foreach (ServiceController serviceOracle in servicesOracle)
+            {
+                string nomService = serviceOracle.ServiceName;
+                string typeDemarrage;
+                string serviceController = "C:\\Windows\\System32\\sc.exe";
+                string commandLine;
+
+                if (serviceActifAuDemarrage == false)
+                    typeDemarrage = "auto";
+                else
+                    typeDemarrage = "disabled";
+
+                commandLine = string.Format("config {0} start= {1}", nomService, typeDemarrage);
+                Process.Start(serviceController, commandLine);
+            }
+
+            Application.Exit();
+        }
+
+
+        private void btn_etat_Click(object sender, EventArgs e)
+        {
+            var servicesOracle =
+                from sc in ServiceController.GetServices()
+                where sc.ServiceName.StartsWith("OracleOra") || sc.ServiceName.Equals("OracleServiceXE")
+                select sc;
+
+            foreach (ServiceController service in servicesOracle)
+            {
+                if (serviceActif == true)
+                    service.Stop();
+                else
+                    service.Start();
+            }
+
+            Application.Exit();
+        }
+    }
+}
